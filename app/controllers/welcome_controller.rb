5d877cce6e3f4cec047c0d3ec1815543cae38f93
ravenusmc <mcuddy77@gmail.com>
2016-01-13 16:16:31 -0500

    got weather data coming in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   def test
+    #The below line gets the response from the API at the below web address and saves it as the variable response.
+    response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/GA/Atlanta.json")
+
+    #Response then pulls out the required information from the JSON data and saves it as an instance variable. 
+    @location = response['location']['city']
+    @temp_f = response['current_observation']['temp_f']
+    @temp_c = response['current_observation']['temp_c']  
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'welcome/test'
+  root 'welcome#test'
 
   get 'welcome/index'
 
