7ba6081a095433a108e959f501658d30ac742aa8
ravenusmc <mcuddy77@gmail.com>
2016-01-17 09:55:52 -0500

    working on weather app

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -7,29 +7,29 @@ class WelcomeController < ApplicationController
 
     response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/#{params[:state]}/#{params[:city]}.json")
     
-    @location = response['location']['city']
-    @temp_f = response['current_observation']['temp_f']
-    @temp_c = response['current_observation']['temp_c']
-    @weather_icon = response['current_observation']['icon_url']
-    @weather_words = response['current_observation']['weather']
-    @forecast_link = response['current_observation']['forecast_url']
-    @real_feel = response['current_observation']['feelslike_f']
+      @location = response['location']['city']
+      @temp_f = response['current_observation']['temp_f']
+      @temp_c = response['current_observation']['temp_c']
+      @weather_icon = response['current_observation']['icon_url']
+      @weather_words = response['current_observation']['weather'] 
+      @forecast_link = response['current_observation']['forecast_url']
+      @real_feel = response['current_observation']['feelslike_f']
 
-    if @weather_words.downcase == "partly cloudy" || @weather_words.downcase == "mostly cloudy"
-      @body_class = "partly-cloudy"
-    elsif @weather_words == "Cloudy" || @weather_words == "Scattered Clouds" || @weather_words == "Overcast"
-      @body_class = "partly-cloudy" 
-    elsif @weather_words.downcase == "clear"
-      @body_class = "sunny"
-    elsif @weather_words.downcase == "snow"
-      @body_class = "snow"
-    elsif @weather_words == "Rain"
-      @body_class = "rain"
-    elsif @weather_words == "Fog"
-      @body_class = "fog"
-    elsif @weather_words == "Thunderstorms and Rain" || @weather_words == "Thunderstorms"
-      @body_class = "thunder"
-    end 
+      if @weather_words == "partly cloudy" || @weather_words == "Mostly Cloudy"
+        @body_class = "partly-cloudy"
+      elsif @weather_words == "Cloudy" || @weather_words == "Scattered Clouds" || @weather_words == "Overcast"
+        @body_class = "partly-cloudy" 
+      elsif @weather_words == "clear"
+        @body_class = "sunny"
+      elsif @weather_words == "snow"
+        @body_class = "snow"
+      elsif @weather_words == "Rain"
+        @body_class = "rain"
+      elsif @weather_words == "Fog"
+        @body_class = "fog"
+      elsif @weather_words == "Thunderstorms and Rain" || @weather_words == "Thunderstorms"
+        @body_class = "thunder"
+      end
   end
 
   def test
